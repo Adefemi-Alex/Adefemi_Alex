feb982aa2bc68d2fe19f48ad4c5b4d46935882ca
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-16 00:42:33 +0100

    fix: udated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
                 <div className="lg:pl-20">
                   <div className="max-w-xs px-2.5 lg:max-w-none">
                     <img
-                      alt="Portrait of Kathan"
+                      alt="Alex Adefemi"
                       src={portraitImage}
                       className="aspect-square rotate-3 border-2 border-dotted rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
                       loading="lazy"
@@ -23,7 +23,7 @@ const About = () => {
 
                 <div className="lg:order-first lg:row-span-2">
                   <h1 className="text-4xl font-bold tracking-tight text-zinc-100 sm:text-5xl"> {/*text-zinc-800 */}
-                    I’m Alex. A Problem Solver, Leader, and software enthusiast.
+                    I’m Alex. Leader, and software dev.
                   </h1>
                   <div className="mt-6 space-y-7 text-base text-zinc-400 tracking-wide"> {/*text-zinc-600 */}
                     <p>
@@ -35,11 +35,11 @@ const About = () => {
                     <p>
                       <b>Areas of Expertise:</b>
                     </p>
-                    <p>Software Engineering, Full Stack Development, Frontend Development, Web Development,Mobile Development, DevOps</p>
+                    <p>Software Engineering, Full Stack Development,Frontend Development, Web Development,Mobile Development</p>
                     <p>
                       <b>Industry Experience:</b>
                     </p>
-                    <p>Software as a Service (SaaS), Fintech, Information Technology, Consulting</p>
+                    <p>Software as a Service (SaaS), Fintech, Information Technology</p>
                     <p>Feel free to reach out to me.</p>
                     <p>Have a great day!</p>
                   </div>
@@ -47,7 +47,7 @@ const About = () => {
                 <div className="lg:pl-20">
                   <ul role="list">
                     <li className="mt-4 flex">
-                      <a className="group flex text-sm font-medium  transition hover:text-teal-500 text-zinc-200 dark:hover:text-teal-500" href="https://github.com/DarkInventor"> {/*text-zinc-800 */}
+                      <a className="group flex text-sm font-medium  transition hover:text-teal-500 text-zinc-200 dark:hover:text-teal-500" href="https://github.com/Adefemi-Alex"> {/*text-zinc-800 */}
                         <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500">
                           <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.475 2 2 6.588 2 12.253c0 4.537 2.862 8.369 6.838 9.727.5.09.687-.218.687-.487 0-.243-.013-1.05-.013-1.91C7 20.059 6.35 18.957 6.15 18.38c-.113-.295-.6-1.205-1.025-1.448-.35-.192-.85-.667-.013-.68.788-.012 1.35.744 1.538 1.051.9 1.551 2.338 1.116 2.912.846.088-.666.35-1.115.638-1.371-2.225-.256-4.55-1.14-4.55-5.062 0-1.115.387-2.038 1.025-2.756-.1-.256-.45-1.307.1-2.717 0 0 .837-.269 2.75 1.051.8-.23 1.65-.346 2.5-.346.85 0 1.7.115 2.5.346 1.912-1.333 2.75-1.05 2.75-1.05.55 1.409.2 2.46.1 2.716.637.718 1.025 1.628 1.025 2.756 0 3.934-2.337 4.806-4.562 5.062.362.32.675.936.675 1.897 0 1.371-.013 2.473-.013 2.82 0 .268.188.589.688.486a10.039 10.039 0 0 0 4.932-3.74A10.447 10.447 0 0 0 22 12.253C22 6.588 17.525 2 12 2Z"></path>
                         </svg>
