49e4f1dd1798e021db243d8ad8ed19de44f6b9df
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-16 00:44:06 +0100

    fix: updated projects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import skillsData from '../api/skillsData'
+import React from 'react';
+import projectsData from '../api/projects';
 
 const Projects = () => {
   return (
-    <section className=' relative' id='projects'>
-    <h2 className='pl-9 text-4xl font-bold text-center text-zinc-100'>Projects</h2>
-    <div className="px-9 py-16 mt-24 md:mt-0 h-full ">
-        <div className="mt-6 grid grid-cols-3  gap-8">
-          {skillsData.map(item => (
-            <div key={item.name} className="w-full  text-zinc-300 bg-zinc-800 py-4 px-3 ">
-              <div className="w-12 h-auto rounded-full ring-1 ring-zinc-500  ">
-                <img src={item.img} className='w-full h-full p-2 object-cover rounded-full bg-zinc-700 ' alt="" />
+    <section className='relative' id='projects'>
+      <h2 className='pl-9 text-4xl font-bold text-center text-zinc-100'>Projects</h2>
+      <div className="px-9 py-16 mt-24 md:mt-0 h-full">
+        <div className="mt-6 grid grid-cols-3 gap-8">
+          {projectsData.map(item => (
+            <div key={item.name} className="w-full text-zinc-300 bg-zinc-800 py-4 px-3">
+              <div className="w-12 h-12 rounded-full ring-1 ring-zinc-500">
+                <img src={item.img} className='w-full h-full p-2 object-cover rounded-full bg-zinc-700' alt={item.name} />
               </div>
               <div className="flex justify-between mt-4">
-              <p className='font-semibold text-md '>{item.name}</p>
-              <p className='font-semibold text-[0.75rem] text-zinc-500'>{item.date}</p>
+                <p className='font-semibold text-md'>{item.name}</p>
+                <a href={item.link} target="_blank" rel="noopener noreferrer" className='font-semibold text-md text-zinc-500 hover:text-white'>
+                  View
+                </a>
               </div>
-              <div className="py-6 px-2">
-              {item.info}
+              <div className="py-6  ">
+                {item.info}
               </div>
             </div>
           ))}
         </div>
-    </div>
-  </section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
