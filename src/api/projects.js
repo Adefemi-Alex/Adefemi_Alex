f4af4da62447723a09706346308673135c627d42
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-16 00:41:35 +0100

    feat: project data

---
+++ src/api/projects.js
@@ -0,0 +1,25 @@
+import keystro from "../assets/projects/keystro.png";
+import artify from "../assets/projects/artify.png";
+
+export const projectsData = [
+    {
+        name: 'Keystro',
+        img: keystro,
+        info: 'Keystro is an innovative eCommerce platform designed to simplify online shopping. It features a user-friendly interface that allows customers to easily browse, search, and purchase products. With secure payment options and a streamlined checkout process, Keystro ensures a seamless shopping experience, making it a go-to solution for both shoppers and merchants alike.',
+        link: 'https://keystro.vercel.app/',
+    },
+    {
+        name: 'Artify Hub',
+        img: artify,
+        info: 'Artify Hub serves as a dynamic platform connecting artists with clients seeking creative services. It allows users to showcase their portfolios and discover exciting art projects, fostering collaboration and community engagement. The user-friendly interface simplifies navigation, making it easy to communicate and manage projects, enhancing the creative process for all users involved in art-related endeavors.',
+        link: 'https://artify-hub-one.vercel.app/',
+    },
+    {
+        name: 'Project Three',
+        img: 'https://via.placeholder.com/50', // Replace with actual logo URLs
+        info: 'A brief description of Project Three.',
+        link: 'https://example.com/project-three',
+    },
+];
+
+export default projectsData;
