bddd016fa6874f3b1a486aaaf6c138c2344d0ffd
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-19 13:06:22 +0100

    update: Initialise aos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Loader from './components/Loader/Loader';
 import './App.css';
+import Aos from 'aos';
 
 const Layout = lazy(() => import('./pages/Layout'));
 const Home = lazy(() => import('./pages/Home'));
 
 function App() {
+  useEffect(() => {
+    Aos.init();
+  })
   return (
     <div className='App bg-black md:px-8 lg:px-14 2xl:px-[10%] overflow-hidden'>
       <Suspense fallback={<Loader />}>
