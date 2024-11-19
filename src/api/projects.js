42a2b5cd73d428e0017bbf9578856eee512ed915
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-19 18:05:10 +0100

    fix: rename screenshot

--- src/api/projects.js
+++ src/api/projects.js
@@ -1,13 +1,13 @@
 import keystro from "../assets/projects/keystro.png";
 import artify from "../assets/projects/artify.png";
 import starOrganicFarms from "../assets/projects/star_organic.png"
-import keystroSC from "../assets/projects/keystro_Screenshot.png"
-import artifySC from "../assets/projects/Artify_Screenshot.png"
+import keystroShot from "../assets/projects/keystro_Screenshot.png"
+import artifyShot from "../assets/projects/Artify_Screenshot.png"
 export const projectsData = [
     {
         name: 'Keystro',
         img: keystro,
-        screenshot: keystroSC,
+        screenshot: keystroShot,
         info: 'Keystro is an innovative eCommerce platform designed to simplify online shopping. It features a user-friendly interface that allows customers to easily browse, search, and purchase products. With secure payment options and a streamlined checkout process, Keystro ensures a seamless shopping experience, making it a go-to solution for both shoppers and merchants alike.',
         link: 'https://keystro.vercel.app/',
     },
@@ -15,7 +15,7 @@ export const projectsData = [
     {
         name: 'Artify Hub',
         img: artify,
-        screenshot: artifySC,
+        screenshot: artifyShot,
         info: 'Artify Hub serves as a dynamic platform connecting artists with clients seeking creative services. It allows users to showcase their portfolios and discover exciting art projects, fostering collaboration and community engagement. The user-friendly interface simplifies navigation, making it easy to communicate and manage projects, enhancing the creative process for all users involved in art-related endeavors.',
         link: 'https://artify-hub-one.vercel.app/',
     },
