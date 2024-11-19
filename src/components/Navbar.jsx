dec0f6b287ca7b82aaddb4923fa4203274790bc2
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-19 13:07:55 +0100

    update: import aos and typewriter

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { LuMoonStar } from "react-icons/lu";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { MdAddIcCall } from "react-icons/md";
 import img1 from '../assets/img1.jpg'
+
+import AOS from 'aos';  
+import 'aos/dist/aos.css';
 const Navbar = () => {
   const location = useLocation();
   const [menuOpen, setMenuOpen] = useState(false);
@@ -15,7 +18,8 @@ const Navbar = () => {
   return (
     <nav className='px-6 md:px-12 lg:px-16 flex justify-between items-center py-6'>
       <Link to="/" className="block">
-      <div className="flex space-x-1 items-center md:pl-10">
+      <div className="flex space-x-1 items-center md:pl-10" 
+      data-aos="fade-right">
         <div className="w-11 h-11 bg-zinc-800 rounded-full">
           <img src={img1} className='rounded-full w-full p-1 border-zinc-500 object-cover' alt="" />
         </div>
@@ -23,16 +27,16 @@ const Navbar = () => {
 
       </div>
       </Link>
-        <ul className='hidden md:flex rounded-full  px-3 space-x-4 font-medium  shadow-lg shadow-zinc-800/5 ring-1  backdrop-blur bg-zinc-800/90 text-zinc-200 ring-white/10'>  
+        <ul className='hidden md:flex rounded-full  px-3 space-x-4 font-medium  shadow-lg shadow-zinc-800/5 ring-1  backdrop-blur bg-zinc-800/90 text-zinc-200 ring-white/10'
+        data-aos="fade-down">  
         {/* bg-white/90 ring-zinc-900/5 text-zinc-800*/}
             {navs.map(({name, link}) =>{
               return (
 
                 <li key={link}>
-                    <a href={link} className={`relative block px-3 text-sm py-2 transition hover:text-teal-500 dark:hover:text-teal-400  ${location.pathname == link && "text-teal-400"}`}>{name}
+                  <a href={link} className={`relative block px-3 text-sm py-2 transition hover:text-teal-500 dark:hover:text-teal-400  ${location.pathname == link && "text-teal-400"}`}>{name}
                     {location.pathname == link && <span class="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0"></span>}
-                    </a>
-                    
+                  </a>
                 </li>
           )
             })}
