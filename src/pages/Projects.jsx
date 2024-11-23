e772874dbc3cbe3873d1c01fec3f7210f3b14fb6
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-23 10:07:26 +0100

    update; add projects and layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import projectsData from '../api/projects';
 
+import { FaGithub } from "react-icons/fa";
+import { IoEyeSharp } from "react-icons/io5";
+
+import 'aos/dist/aos.css';
 const Projects = () => {
   return (
     <section className='relative' id='projects'>
       <h2 className='pl-9 text-4xl font-bold text-center text-zinc-100'>Projects</h2>
-      <div className="px-9 py-16 mt-24 md:mt-0 h-full">
-        <div className="mt-6 grid grid-cols-2 gap-8">
-          {projectsData.map(item => (
-            <div key={item.name} className="w-full relative">
-              <div className="w-full">
+      <div className="px-9 py-2 md:py-16 mt-10 md:mt-0 h-full">
+        <div className="mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projectsData.map((item, index) => (
+            <div key={item.name} className="w-full relative" data-aos="fade-up" data-aos-delay={150 *index}>
+              <div className="w-full h-56 md:h-auto">
                 <img src={item.screenshot} alt={item.name} className='w-full h-full object-contain rounded-[12px] rounded-b-none' />
               </div>
-              <div className="w-12 h-12 rounded-full ring-1 ring-zinc-500 absolute -bottom-2 -left-4">
+              <div className="w-12 h-12 rounded-full ring-1 ring-zinc-500 absolute -top-2 -left-4">
                 <img src={item.img} className='w-full h-full p-2 object-cover rounded-full bg-zinc-700' alt={item.name} />
               </div>
               
-              {/* <div className="flex justify-between mt-4  text-zinc-300 bg-zinc-800">
+              <div className="text-zinc-300 bg-zinc-800 px-5 py-5">
                 <p className='font-semibold text-md'>{item.name}</p>
-                <a href={item.link} target="_blank" rel="noopener noreferrer" className='font-semibold text-md text-zinc-500 hover:text-white'>
-                  View
+                <div className="flex justify-between mt-2">
+                <a href={item.link} target="_blank" rel="noopener noreferrer" className='font-semibold text-md text-zinc-500  hover:text-white flex space-x-2'>
+                  <p className="text-nowrap">Code</p>
+                  <FaGithub size={20} />
+                  
                 </a>
-              </div> */}
+                <a href={item.link} target="_blank" rel="noopener noreferrer" className='font-semibold text-md text-zinc-500  hover:text-white flex space-x-2'>
+                  <p className="text-nowrap">View </p>
+                  <IoEyeSharp size={24} />
+                  
+                  
+                </a>
+
+                </div>
+              </div>
               {/* <div className="py-6  ">
                 {item.info}
               </div> */}
