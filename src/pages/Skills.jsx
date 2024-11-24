33063e50ea62f7488177d3750ba2a59b0612c9ec
Adefemi-Alex <121715954+Adefemi-Alex@users.noreply.github.com>
2024-11-24 19:15:53 +0100

    fix: remove mobile animation

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import skillsData from '../api/skillsData';
-
+import { useWindowSize } from 'react-haiku';
 const Skills = () => {
+  const { width } = useWindowSize();
   return (
-    <section className='gradient-sides relative' id='skills'>
+    <section className={`${width >= 768 && "gradient-sides"}  relative py-16 md:py-0`} id='skills'>
       <h2 className='pl-9 text-4xl font-bold text-center text-zinc-100'>Skills</h2>
-      <div className="px-9 py-16 mt-24 md:mt-0 h-full overflow-hidden">
-        <div className="scrolling-carousel whitespace-nowrap space-x-10 animate-scroll">
+      <div className="px-9  md:py-16 mt-7 md:mt-0 h-full overflow-hidden">
+        <div className={`${width >= 768 && "scrolling-carousel"} grid md:flex grid-cols-2 gap-4 whitespace-nowrap md:space-x-10 animate-scroll`}>
           {skillsData.slice(0, 8).map(item => (
-            <div key={item.name} className="inline-block rounded-full text-zinc-300 bg-zinc-800 py-3 px-5  transition-transform transform hover:scale-105">
+            <div key={item.name} className="inline-block md:rounded-full text-zinc-300 bg-zinc-800 py-3 px-5  transition-transform transform hover:scale-105"
+            >
               <div className="flex justify-start items-center">
                 <div className="w-9 h-9  overflow-hidden">
                   <img src={item.img} className='w-full h-full p-1 object-cover' alt={item.name} />
@@ -18,9 +20,9 @@ const Skills = () => {
             </div>
           ))}
         </div>
-        <div className="scrolling-carousel-rev whitespace-nowrap space-x-10 mt-2 animate-scroll">
+        <div className={`${width >= 768 && "scrolling-carousel-rev"} grid md:flex grid-cols-2 gap-4 whitespace-nowrap md:space-x-10 mt-2 animate-scroll`}>
           {skillsData.slice(8, skillsData.length).map(item => (
-            <div key={item.name} className="inline-block rounded-full text-zinc-300 bg-zinc-800 py-3 px-5  transition-transform transform hover:scale-105">
+            <div key={item.name} className="inline-block md:rounded-full text-zinc-300 bg-zinc-800 py-3 px-5  transition-transform transform hover:scale-105">
               <div className="flex justify-start items-center">
                 <div className="w-9 h-9  overflow-hidden">
                   <img src={item.img} className='w-full h-full p-1 object-cover' alt={item.name} />
